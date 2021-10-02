e8939a2bd5b94c0bb38923f4c83c2b16ebde4e5b
Jeon BM <Jeon BM@211.177.240.231>
2021-10-03 00:28:15 +0900

    fg

---
+++ ghjdgj/src/ghjdgj/Google.java
@@ -0,0 +1,10 @@
+package ghjdgj;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ghjdgj/src/ghjdgj/Naver.java
@@ -0,0 +1,10 @@
+package ghjdgj;
+
+public class Naver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
