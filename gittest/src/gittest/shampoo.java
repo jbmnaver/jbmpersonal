91dc4a8274e932c2f260dd3df5b71e2c8773ba0d
smhrd <smhrd@220.80.33.3>
2021-10-02 23:58:28 +0900

    ㅖ;'

--- gittest/src/gittest/shampoo.java
+++ gittest/src/gittest/shampoo.java
@@ -4,18 +4,8 @@ package gittest;
 public class shampoo {
 
 	public static void main(String[] args) {
-		System.out.println("가나다라마바사");
+	
 		
-		String str = "gmagma";
-		System.out.println(str);
-	}
-
-	public int a;
-	public int b;
-	
-	public int plus(int a,int b) {
-		int result =  a+b;
-		return result;
-	}
-	
+		
+}
 }
