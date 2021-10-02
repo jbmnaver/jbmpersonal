34ef5ed389be0a910ddd6ca3aa164c8ed5137a00
Jeon BM <Jeon BM@211.177.240.231>
2021-10-02 22:10:01 +0900

    first

---
+++ gittest/src/gittest/shampoo.java
@@ -0,0 +1,26 @@
+package gittest;
+
+import java.util.Scanner;
+
+public class shampoo {
+
+	public static void main(String[] args) {
+		System.out.println("가나다라마바사");
+		
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		
+
+	}
+
+	public int a;
+	public int b;
+	
+	public int plus(int a,int b) {
+		int result =  a+b;
+		return result;
+	}
+	
+}
